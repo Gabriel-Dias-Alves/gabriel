53aa35ac3a2e104d9188adc206bd895705c905eb
Gabriel Dias Alves <gabrielalvesrad@gmail.com>
2026-08-20 23:49:09 +0000

    negobom

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 nome="Gabriel"
+negobom=2,50
+comprar=50*negobom
 nota1= 8.5
 nota2= 7.0
 media = (nota1 + nota2) / 2
@@ -6,3 +8,4 @@ media = (nota1 + nota2) / 2
 if (media >= 7) {
     console.log("Aprovado" + media)
 }
+console.log("O valor do negobom é: R$"+comprar)
